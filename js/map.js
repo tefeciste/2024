d19307c9946c5b09fc1674d9dac1074575238c0a
luccl <tefeciste@hotmail.com>
2019-11-24 16:42:27 +0100

    local

--- js/map.js
+++ js/map.js
@@ -800,12 +800,12 @@ $( document ).ready(function() {
     $('#g0').click(function () {
         if (isGroupShow[0]===true){
             for (var j=0, length = groupListArray[j].length; j< length-1; j++) {
-                map.removeLayer(groupListArray[index][j]);
+                map.removeLayer(groupListArray[0][j]);
             }
             isGroupShow[0]=false;
         }else{
             for (var j=0, length = groupListArray[j].length; j< length-1; j++){
-                groupListArray[index][j].addTo(map);
+                groupListArray[0][j].addTo(map);
             }
             isGroupShow[0]=true;
         }
