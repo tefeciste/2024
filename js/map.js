40848b6a94ebc8ef37291288eec2e239a4b35995
luccl <tefeciste@hotmail.com>
2019-11-26 23:42:09 +0100

    change

--- js/map.js
+++ js/map.js
@@ -666,17 +666,13 @@ var baseLayers = {
 //		Panel Layers
 var lay = L.control.layers.tree(baseLayers, overLayers,
     {
-        namedToggle: false,
-        selectorBack: false,
-        closedSymbol: '&#8862; &#x1f5c0;',
-        openedSymbol: '&#8863; &#x1f5c1;',
+        namedToggle: true,
+        collapseAll: 'Collapse all',
+        expandAll: 'Expand all',
         collapsed: false,
     });
 
 lay.addTo(map).collapseTree().expandSelected().collapseTree(true);
-L.DomEvent.on(L.DomUtil.get('onlysel'), 'click', function () {
-    lay.collapseTree(true).expandSelected(true);
-});
 
 //	Add Scale control to map
 L.control.scale({imperial: false}).addTo(map);
